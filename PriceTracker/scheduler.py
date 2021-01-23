67e21a5ea084e8e5ff1325937dc6c2c66f48d88d
Adit <aditshah2002@gmail.com>
2021-01-23 22:04:44 +0530

    fix: multiple execution of scheduled job

--- PriceTracker/scheduler.py
+++ PriceTracker/scheduler.py
@@ -2,7 +2,9 @@ import logging
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.executors.pool import ProcessPoolExecutor, ThreadPoolExecutor
-from django_apscheduler.jobstores import register_events, register_job
+from django_apscheduler.jobstores import register_job, register_events
+from apscheduler.triggers.cron import CronTrigger
+
 
 from django.conf import settings
 
@@ -158,9 +160,15 @@ def start():
     # - Add a scheduled job to the job store on application initialization
     # - The job will execute a model class method at midnight each day
     # - replace_existing in combination with the unique ID prevents duplicate copies of the job
-    #scheduler.add_job(Check_Prices, "cron", id="my_class_method", hour=0, replace_existing=True)
-    scheduler.add_job(Check_Prices, 'interval', hours=24, name='clean_accounts', jobstore='default',id="my_class_method",replace_existing=True)
-
+    #scheduler.add_job(Check_Prices, "cron", id="my_class_method",h , replace_existing=True)
+    #scheduler.add_job(Check_Prices, 'interval', hours=24, name='clean_accounts', jobstore='default',id="my_class_method",replace_existing=True)
+    scheduler.add_job(
+        Check_Prices,
+        trigger=CronTrigger(minute="*/5"),  # Every 5 minutes
+        id="my_class_method",  # The `id` assigned to each job MUST be unique
+        max_instances=1,
+        replace_existing=True,
+    )
     # Add the scheduled jobs to the Django admin interface
     register_events(scheduler)
 
